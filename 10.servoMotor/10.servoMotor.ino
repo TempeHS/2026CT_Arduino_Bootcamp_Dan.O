0e5102ce34d6c63a1072b6c0bdaa58ae9f971043
Dan O <dan.ozeri@education.nsw.gov.au>
2025-08-25 12:47:23 +1000

    tehcniaacal
    s=buh

--- 10.servoMotor/10.servoMotor.ino
+++ 10.servoMotor/10.servoMotor.ino
@@ -21,13 +21,39 @@
     https://www.tinkercad.com/things/lQ9RyYJRoLn?sharecode=MKlN0A7R0WGodkdTRKkPJO7I8PeI5L_GCR7pCclQ0qM
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/10.servoMotor/Bootcamp-servoMotor.png
 */
+//Includes or OLED screen
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #include <Servo.h>
 
+#include "Ultrasonic.h"
+Ultrasonic myUltraSonicSensor(5);
+
+unsigned static int servoPin = 6;
+
+Servo myservo;
+
+int val;
+int potpin = A1;
+
+
 void setup() {
-  
+  myservo.attach(servoPin);
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+
+  unsigned long RangeInCentimeters;
+  RangeInCentimeters = myUltraSonicSensor.distanceRead();
+  val = map(RangeInCentimeters,0, 30, 0, 180);
+  myservo.write(val);
+  delay(15);
+
+  Serial.print(RangeInCentimeters);
+  Serial.println(" cm");
+  delay(250);
 }
--- 11.ultrasonicSensor/11.ultrasonicSensor.ino
+++ 11.ultrasonicSensor/11.ultrasonicSensor.ino
@@ -24,12 +24,18 @@
 */
 
 #include "Ultrasonic.h"
-
+Ultrasonic myUltraSonicSensor(5);
 void setup()
 {
-  
+  Serial.begin(9600);
 }
 void loop()
 {
+  unsigned long RangeInCentimeters;
+  RangeInCentimeters = myUltraSonicSensor.distanceRead();
+  Serial.print(RangeInCentimeters);
+  Serial.println(" cm");
+  delay(250);
+
   
 }
