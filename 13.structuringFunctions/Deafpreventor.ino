ba813593ddddfcba55833943ca47248690b2b941
Dan O <dan.ozeri@education.nsw.gov.au>
2025-09-10 11:53:47 +1000

    deaf
    f

--- 13.structuringFunctions/Deafpreventor.ino
+++ 13.structuringFunctions/Deafpreventor.ino
@@ -1,73 +1,26 @@
-/\*
 
-- Square wave tune with an Arduino and a PC speaker.
-- The calculation of the tones is made following the mathematical
-- operation:
--
-- timeUpDown = 1/(2 \* toneFrequency) = period / 2
-- )c( Copyleft 2009 Daniel Gimpelevich
-- Inspired from AlexandreQuessy's https://playground.arduino.cc/Code/MusicalAlgoFun
-  \*/
+// Arduino's pin connected to OUT pin of the sound sensor
+#define SENSOR_PIN 5
 
-const byte ledPin = 13;
-const byte speakerOut = 11; /_ This makes a standard old PC speaker connector fit nicely over the pins. _/
-
-/_ 10.5 octaves :: semitones. 60 = do, 62 = re, etc. _/
-/_ MIDI notes from 0, or C(-1), to 127, or G9. _/
-/_ Rests are note number -1. _/
-
-unsigned int timeUpDown[128];
-
-/_ our song. Each number pair is a MIDI note and a note symbol. _/
-/_ Symbols are 1 for whole, -1 for dotted whole, 2 for half, _/
-/_ -2 for dotted half, 4 for quarter, -4 for dotted quarter, etc. _/
-
-const byte BPM = 120;
-const char song[] = {
-64,4,64,4,65,4,67,4, 67,4,65,4,64,4,62,4,
-60,4,60,4,62,4,64,4, 64,-4,62,8,62,2,
-64,4,64,4,65,4,67,4, 67,4,65,4,64,4,62,4,
-60,4,60,4,62,4,64,4, 62,-4,60,8,60,2,
-62,4,62,4,64,4,60,4, 62,4,64,8,65,8,64,4,60,4,
-62,4,64,8,65,8,64,4,62,4, 60,4,62,4,55,2,
-64,4,64,4,65,4,67,4, 67,4,65,4,64,4,62,4,
-60,4,60,4,62,4,64,4, 62,-4,60,8,60,2};
-
-int period, i;
-unsigned int timeUp, beat;
-byte statePin = LOW;
-const float TEMPO_SECONDS = 60.0 / BPM;
-const unsigned int MAXCOUNT = sizeof(song) / 2;
+int lastState = HIGH;  // the previous state from the input pin
+int currentState;      // the current reading from the input pin
 
 void setup() {
-pinMode(ledPin, OUTPUT);
-pinMode(speakerOut, OUTPUT);
-for (i = 128; i--;)
-timeUpDown[i] = 1000000 / (pow(2, (i - 69) / 12.0) \* 880);
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the Arduino's pin as an input
+  pinMode(SENSOR_PIN, INPUT);
 }
 
 void loop() {
-digitalWrite(speakerOut, LOW);
- for (beat = 0; beat < MAXCOUNT; beat++) {
-statePin = !statePin;
-digitalWrite(ledPin, statePin);
+  // read the state of the the input pin:
+  currentState = digitalRead(SENSOR_PIN);
 
-    i = song[beat * 2];
-    timeUp = (i < 0) ? 0 : timeUpDown[i];
+  if (lastState == HIGH && currentState == LOW)
+    Serial.println("The sound has been detected");
+  else if (lastState == LOW && currentState == HIGH)
+    Serial.println("The sound has disappeared");
 
-    period = (timeUp ? (1000000 / timeUp) / 2 : 250) * TEMPO_SECONDS
-      * 4 / song[beat * 2 + 1];
-    if (period < 0)
-      period = period * -3 / 2;
-    for (i = 0; i < period; i++) {
-      digitalWrite(speakerOut, timeUp ? HIGH : LOW);
-      delayMicroseconds(timeUp ? timeUp : 2000);
-      digitalWrite(speakerOut, LOW);
-      delayMicroseconds(timeUp ? timeUp : 2000);
-    }
-    delay(50);
-
-}
-digitalWrite(speakerOut, LOW);
-delay(1000);
+  // save the the last state
+  lastState = currentState;
 }
